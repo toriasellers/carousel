08f64b41124b761c3f1f2cff9d0f9ff951df7b57
toria <114430418+toriasellers@users.noreply.github.com>
2023-07-25 02:36:13 -0400

    Add files via upload

---
+++ carousel/carousel/src/Carousel.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./Carousel.css";
+import Card from "./Card";
+
+ function Carousel({ photos, title }) {
+  const [currCardIdx, setCurrCardIdx] = useState(0);
+
+  const currCard = photos[currCardIdx];
+  const total = photos.length;
+
+  //Increments currCardIdx state by 1
+  function goForward() {
+    setCurrCardIdx(currCardIdx + 1);
+  }
+
+  return (
+    <div className="Carousel">
+      <h1>{title}</h1>
+      <div className="Carousel-main">
+        <i
+          className="bi bi-arrow-left-circle"
+          onClick={goForward}
+        />
+        <Card
+          caption={currCard.caption}
+          src={currCard.src}
+          currNum={currCardIdx + 1}
+          totalNum={total}
+        />
+        <i
+          className="bi bi-arrow-right-circle"
+          onClick={goForward}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
